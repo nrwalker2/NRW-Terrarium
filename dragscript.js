58cf9e73334a2722748ef2e32acda7681c8a055c
nrwalker2 <135749410+nrwalker2@users.noreply.github.com>
2024-07-07 21:43:33 -0500

    Update dragscript.js

--- dragscript.js
+++ dragscript.js
@@ -1,5 +1,5 @@
-/*The solution to draggable elements was inspired by w3schools solution on creating a [Draggable HTML Element](https://www.w3schools.com/howto/howto_js_draggable.asp).*/
-
+/* Nolan Walker drag script*/
+/* creating elements and assinging them to the plant images*/
 dragElement(document.getElementById('plant1'));
 dragElement(document.getElementById('plant2'));
 dragElement(document.getElementById('plant3'));
@@ -15,11 +15,7 @@ dragElement(document.getElementById('plant12'));
 dragElement(document.getElementById('plant13'));
 dragElement(document.getElementById('plant14'));
 
-/*"A closure is the combination of a function bundled together (enclosed) 
-with references to its surrounding state (the lexical environment). 
-In other words, a closure gives you access to an outer function’s scope 
-from an inner function." Create a closure so that you can track the dragged element*/
-
+/* creating drag element functions */
 function dragElement(terrariumElement) {
 	//set 4 positions for positioning on the screen
 	let pos1 = 0,
